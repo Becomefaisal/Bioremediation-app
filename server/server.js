8d8dc03e36231ccd1e030a3ba3e3a83e649ffd32
becomefaisal <fsajjad007@gmail.com>
2026-05-19 23:40:58 +0530

    cors fix 2.0

--- server/server.js
+++ server/server.js
@@ -4,20 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-  origin: function(origin, callback) {
-    // Allow all Vercel preview/production domains and localhost
-    if (!origin ||
-        origin.endsWith('.vercel.app') ||
-        origin === 'https://bioremediation-app.vercel.app' ||
-        origin.startsWith('http://localhost')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
